29b2e763a06deeecd0b2b3c921f50b191cc3c7ad
Tulio.Sploradori <tulio.sploradori@ab-inbev.com>
2026-04-07 19:06:57 -0300

    Modelagem de Dados

---
+++ src/main/java/br/com/zenon/Main.java
@@ -0,0 +1,43 @@
+package br.com.zenon;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Transaction(
+            1,
+            TransactionType.PAYMENT,
+            new BigDecimal("9839.64"),
+            new TransactionCustomer(
+                "C1231006815",
+                new BigDecimal("170136.0"),
+                new BigDecimal("160296.36")
+            ),
+            new TransactionCustomer(
+                "M1979787155",
+                new BigDecimal("0.0"),
+                new BigDecimal("0.0")
+            ),
+            false,
+            true
+        ));
+
+        System.out.println(new Transaction(
+            2,
+            TransactionType.CASH_OUT,
+            new BigDecimal("850002.52"),
+            new TransactionCustomer(
+                "C1280323807",
+                new BigDecimal("850002.52"),
+                new BigDecimal("0.0")
+            ),
+            new TransactionCustomer(
+                "C873221189",
+                new BigDecimal("6510099.11"),
+                new BigDecimal("7360101.63")
+            ),
+            true,
+            false
+        ));
+    }
+}
---
+++ src/main/java/br/com/zenon/TransactionIngestor.java
@@ -0,0 +1,11 @@
+package br.com.zenon;
+
+import java.util.List;
+
+public class TransactionIngestor {
+
+    List<Transaction> listTransaction(String fileName) {
+
+        return List.of();
+    }
+}
