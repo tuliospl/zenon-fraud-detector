c2f421ebac0cb09e5a7e36584faa8eaf63610bca
Tulio.Sploradori <tulio.sploradori@ab-inbev.com>
2026-04-20 12:48:04 -0300

    tratamento-erros

--- src/main/java/br/com/zenon/Transaction.java
+++ src/main/java/br/com/zenon/Transaction.java
@@ -1,6 +1,7 @@
 package br.com.zenon;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record Transaction(
     int step,
@@ -12,4 +13,15 @@ public record Transaction(
 
     boolean isFraud,
     boolean isFlaggedFraud
-) {}
+) {
+    public Transaction {
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(origin);
+        Objects.requireNonNull(recipient);
+        Objects.requireNonNull(amount);
+
+        if (step <= 0) throw new IllegalArgumentException("O valor de step deve ser maior ou igual a zero");
+        if (amount.signum() <= 0) throw new IllegalArgumentException("O valor de amount deve ser maior ou igual a zero");
+
+    }
+}
